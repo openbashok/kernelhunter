bb2e1d6cb60cb61e0b68e37a1709ca14c44fa9ff
openbashok <msomoza@openbash.com>
2025-06-01 19:21:51 -0300

    Update kernelHunter.py

--- kernelHunter.py
+++ kernelHunter.py
@@ -28,6 +28,7 @@ from external_adn import get_random_fragment_from_bin
 from extract_function_adn import get_function_or_fragment
 from filesystem_chaos import generate_filesystem_chaos_fragment
 from gene_bank import get_random_gene
+from gene_bank_advanced_v2 import get_random_gene_dynamic
 
 def format_shellcode_c_array(shellcode_bytes):
     return ','.join(f'0x{b:02x}' for b in shellcode_bytes)
@@ -242,6 +243,7 @@ def generate_random_instruction():
         "function_adn",
         "filesystem_chaos",
         "gene_bank",
+        "gene_bank_dynamic",
     ]
 
     #weights = [100, 0, 0, 0, 0, 0, 0, 0, 0]  # Probabilidades relativas
@@ -249,7 +251,7 @@ def generate_random_instruction():
     #weights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,100]
     #weights = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 15, 5, 5, 5]
     #weights = [5, 3, 2, 5, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,75]
+    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,74]
 
     choice_type = random.choices(options, weights=weights)[0]
 
@@ -317,7 +319,10 @@ def generate_random_instruction():
     
     elif choice_type == "gene_bank":
         return get_random_gene()
-    
+        
+    elif choice_type == "gene_bank_dynamic":
+        return get_random_gene_dynamic()
+        
     elif choice_type == "memory_access":
         # Instrucciones que acceden a memoria, más probabilidad de fallos
         mem_instructions = [
