5f3981d719b274c9a06c9878583697d0d7bd36d3
openbashok <msomoza@openbash.com>
2025-06-01 21:24:33 -0300

    Update kernelHunter.py

--- kernelHunter.py
+++ kernelHunter.py
@@ -34,6 +34,7 @@ from interrupt_storm import generate_interrupt_storm_fragment
 from inversion_mutation import invert_fragment
 from ipc_stress_attack import generate_ipc_stress_fragment
 from kpti_breaker import generate_kpti_breaker_fragment
+from memory_fragmentation_attack import generate_memory_fragmentation_fragment
 
 def format_shellcode_c_array(shellcode_bytes):
     return ','.join(f'0x{b:02x}' for b in shellcode_bytes)
@@ -253,6 +254,7 @@ def generate_random_instruction():
         "interrupt_storm",
         "ipc_stress",
         "kpti_breaker",
+        "memory_fragmentation",
     ]
 
     #weights = [100, 0, 0, 0, 0, 0, 0, 0, 0]  # Probabilidades relativas
@@ -260,7 +262,7 @@ def generate_random_instruction():
     #weights = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,100]
     #weights = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 15, 5, 5, 5]
     #weights = [5, 3, 2, 5, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,1,1,1,70]
+    weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,1,1,1,1,69]
 
     choice_type = random.choices(options, weights=weights)[0]
 
@@ -341,6 +343,8 @@ def generate_random_instruction():
         return generate_ipc_stress_fragment(min_ops=5, max_ops=15)
     elif choice_type == "kpti_breaker":
         return generate_kpti_breaker_fragment(min_ops=15, max_ops=30)        
+    elif choice_type == "memory_fragmentation":
+        return generate_memory_fragmentation_fragment(min_ops=5, max_ops=15)
     elif choice_type == "memory_access":
         # Instrucciones que acceden a memoria, más probabilidad de fallos
         mem_instructions = [
